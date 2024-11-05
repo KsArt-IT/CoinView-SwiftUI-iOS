01a59dc12f4faf3a1fc8cf663a19739c0f965b43
KsArT-IT <ksart.it@gmail.com>
2024-11-05 10:38:44 +0200

    feat: add coin dto

---
+++ CoinView-SwiftUI/Data/DTO/CoinDto.swift
@@ -0,0 +1,32 @@
+//
+//  CoinDto.swift
+//  CoinView-SwiftUI
+//
+//  Created by KsArT on 05.11.2024.
+//
+
+import Foundation
+
+struct CoinDto: Decodable {
+    let id: String
+    let isActive: Bool
+    let isNew: Bool
+    let name: String
+    let rank: Int
+    let symbol: String
+    let type: String
+}
+
+extension CoinDto {
+    
+    func mapToDomain(_ logo: Data? = nil) -> Coin {
+        Coin(
+            id: self.id,
+            isActive: self.isActive,
+            name: self.name,
+            rank: self.rank,
+            symbol: self.symbol,
+            logo: logo
+        )
+    }
+}
